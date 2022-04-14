9c0d003d02c3b6f5329f6a8271e276ba4beb413b
Black-Loli <pundanya@gmail.com>
2022-04-14 15:33:02 +0300

    Начало работы над выпадающим меню сбоку основной страницы

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>...</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bellota+Text:wght@300;400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+<input type="checkbox" id="ham-menu">
+<label for="ham-menu">
+    <div class="hide-des">
+        <span class="menu-line"></span>
+        <span class="menu-line"></span>
+        <span class="menu-line"></span>
+        <span class="menu-line"></span>
+        <span class="menu-line"></span>
+        <span class="menu-line"></span>
+    </div>
+</label>
+
+<div class="full-page-green"></div>
+<div class="ham-menu">
+    <ul class="centre-text bold-text">
+        <li>Home</li>
+        <li>Categories</li>
+        <li>Services</li>
+        <li>Shop</li>
+        <li>Support</li>
+        <li>Contact us</li>
+    </ul>
+</div>
+</body>
+</html>
